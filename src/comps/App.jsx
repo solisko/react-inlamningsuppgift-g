cece223f2dc5ab3c8a2b146ae2b2f6df36fd06b3
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-18 20:07:06 +0100

    nav start

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -1,12 +1,12 @@
-import '../css/App.css'
+import "../css/App.css";
+import Nav from "./Nav";
 
 function App() {
-
   return (
     <>
-
+      <Nav />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
