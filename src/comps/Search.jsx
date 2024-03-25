2ff65a5c5f2e3b3c5c7892e146d2baef8df6f787
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-25 21:58:39 +0100

    search by ingredient add

--- src/comps/Search.jsx
+++ src/comps/Search.jsx
@@ -7,21 +7,43 @@ export default function Search({ setRecipe, setSearchResult }) {
   const inputRef = useRef();
 
   useEffect(() => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data.meals) {
+    const searchByMeal = fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    });
+
+    const searchByIngredient = fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${input}`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    });
+
+    Promise.all([searchByMeal, searchByIngredient])
+      .then(([mealResult, ingredientResult]) => {
+        const meals = mealResult?.meals || [];
+        const ingredients = ingredientResult?.meals || [];
+
+        const combinedResults = [...meals, ...ingredients];
+
+        const filterdResults = combinedResults.filter(
+          (meal, index, self) =>
+            index === self.findIndex((m) => m.idMeal === meal.idMeal)
+        );
+
+        if (filterdResults.length > 0) {
           // // hämta localStorage, lägg till för varje måltid en rating
           // const ratings = JSON.parse(localStorage.getItem('ratings'))
           // data.meals.forEach(meal => {
           //   meal.rating = ratings.filter(r => r.id === meal.id)
           // });
-          setSearchResult(data.meals);
+          setSearchResult(filterdResults);
           setErrorMsg("");
         } else {
           setSearchResult([]);
