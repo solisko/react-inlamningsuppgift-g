a4cfaaa30a5956bbed61cc6b856925dfa0e387d5
Sofia Skommar <sofiaskommar@gmail.com>
2024-03-25 20:23:44 +0100

    fixing list id

--- src/comps/RecipeList.jsx
+++ src/comps/RecipeList.jsx
@@ -2,16 +2,16 @@ import styles from "../css/recipelist.module.css";
 
 export default function RecipeList({ searchResult, setRecipeId, ratingsComp }) {
   const RatingsComp = ratingsComp;
-  console.log(searchResult)
+  console.log(searchResult);
 
   return (
     <div>
-      {searchResult.map((recipe) => (
-        <div key={recipe.idMeal} className={styles.recipeCard}>
+      {searchResult.map((recipe, index) => (
+        <div key={index} className={styles.recipeCard}>
           <section className={styles.imgSection}>
             <img className={styles.image} src={recipe.strMealThumb} alt="" />
           </section>
-          <div className={styles.textSection} >
+          <div className={styles.textSection}>
             <h2>{recipe.strMeal}</h2>
             <section className={styles.btnSection}>
               <button
